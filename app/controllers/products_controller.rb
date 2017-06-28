5dfafd2a8467d751fe953a9c6a8df2670abab95e
justin <justinng@Justins-MacBook-Air.local>
2017-06-28 23:45:51 +0800

    implement front product index&show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,18 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
+    @products = Product.all
   end
 
   def show
-    product = Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
 
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image)
   end
 
-  
+
 end
