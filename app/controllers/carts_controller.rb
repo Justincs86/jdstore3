a590689243a7c2e01192cdcc089f075b5be7e5cd
justin <justinng@Justins-MacBook-Air.local>
2017-06-30 22:46:58 +0800

    empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,14 @@
 class CartsController < ApplicationController
+
+  def clean
+    current_cart.clean!
+    redirect_to carts_path, warning: "Empty cart"
+  end
+  
+  def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+      redirect_to carts_path
+  end
+
 end
